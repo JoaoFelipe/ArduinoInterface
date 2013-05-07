232358eead1022e529ddb56e22a8f7ae5a236e3f
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2013-05-06 19:38:48 -0500

    Comments

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+"""    main.py
+ Purpose: Final demo
+ Author: Tiago Pimentel
+        t.pimentelms@gmail.com
+         Joao Felipe
+        joaofelipenp@gmail.com
+         Matheus Camargo
+        matheusfc09@gmail.com
+ CSE 467 -- Embedded Computing Systems
+ WUSTL, Spring 2013
+ Date: Apr., 29, 2013
+
+ Description:
+    This is the main of the interface with internet/bluetooth for android
+
+"""
+
 from java.lang import Thread as JThread
 from Arduino import Arduino
 
