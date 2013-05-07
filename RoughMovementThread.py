7bb0734853652db29ca327e740a8dafc455e6985
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2013-05-06 19:53:52 -0500

    Update RoughMovementThread.py

--- RoughMovementThread.py
+++ RoughMovementThread.py
@@ -1,11 +1,26 @@
+"""    RoughMovementThread.py
+ Purpose: Final demo
+ Author: Tiago Pimentel
+        t.pimentelms@gmail.com
+         Joao Felipe
+        joaofelipenp@gmail.com
+         Matheus Camargo
+        matheusfc09@gmail.com
+ CSE 467 -- Embedded Computing Systems
+ WUSTL, Spring 2013
+ Date: Apr., 29, 2013
+
+ Description:
+    Checks if a rough movement occurred and sends to arduino
+
+"""
+
 import httplib
 from java.lang import Runnable
 from java.lang import Thread as JThread
 
 ROUGH_TIME = 10000
 
-# RoughMovementThread
-# checks if a rough movement occurred and sends to arduino
 class RoughMovementThread(Runnable):
     
     def __init__(self, arduino):
